486b32a0b161a9e5b49d86a43009337e3e0c3bc1
Dung <dungqvnc@gmail.com>
2022-03-08 00:32:13 +0700

    oke

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Các vượn chị';
+const giftUrl = 'https://www.youtube.com/watch?v=g3-uN_H5QoE';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = '';
+const contentCard = 'Chúc các chị vượn ra thủ đô thêm lực lưỡng, ăn lắm, khôn ra. Con nào thiếu thì sớm kiếm được 1 chú đực hoang dại để đính kèm đi kiếm ăn cho nhàn';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '83.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
